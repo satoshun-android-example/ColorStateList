0a3f880649f0c6a9ecd30a78725f991339d33ce3
SatoShun <shun.sato1@gmail.com>
2020-03-02 02:47:33 +0000

    update

--- app/src/main/java/com/github/satoshun/example/main/MainFragment.kt
+++ app/src/main/java/com/github/satoshun/example/main/MainFragment.kt
@@ -1,28 +1,16 @@
 package com.github.satoshun.example.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.github.satoshun.example.R
 import com.github.satoshun.example.databinding.MainFragBinding
 
-class MainFragment : Fragment() {
-  private lateinit var binding: MainFragBinding
-
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    binding = MainFragBinding.inflate(inflater, container, false)
-    return binding.root
-  }
-
+class MainFragment : Fragment(R.layout.main_frag) {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    val binding = MainFragBinding.bind(view)
 
     binding.colorStateAlpha.setOnClickListener {
       findNavController().navigate(R.id.nav_color_state_list_alpha)
